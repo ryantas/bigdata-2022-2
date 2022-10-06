0a328c7182210322dde66f1f5fc1b219647aeaca
Richard Yantas <richard@dysopsis.com>
2022-10-06 16:23:35 -0500

    ray-train, ray-rllib and ray-core released

---
+++ Lab06-ray/ray-docker-compose/setup-r.sh
@@ -0,0 +1,13 @@
+
+# ray start --head --redis-port=8888
+ray start --head
+
+# from cli on node
+ray start --address='192.168.0.39:8888' --redis-password='5241590000000000'
+# from python on node
+import ray 
+ray.init(address='auto', redis-password='5241590000000000')
+
+# connect to different computer
+
+
